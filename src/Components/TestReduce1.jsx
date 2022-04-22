7a4445d8c11c3d47916c971b091626a5a7d9e47b
sirfreedom <alemillan22@gmail.com>
2022-04-22 17:52:56 -0300

    Se agrega diseño
    al class

--- src/Components/TestReduce1.jsx
+++ src/Components/TestReduce1.jsx
@@ -1,10 +1,58 @@
 import React, {Fragment} from 'react';
-//import React, {useState, useEffect} from 'react';
+import {Provider} from 'react-redux';
+//import {store} from './store';
+//import {saveState} from './localStorage';
+
+/*
+store.subscribe(() => {
+  saveState({
+    cart: store.getState().cart,
+    total: store.getState().total,
+    amount: store.getState().amount,
+  });
+});
+*/
+
+const cartItems = [
+  {
+    id: 1,
+    title: 'Samsung',
+    price: 799.99,
+    img: 'shorturl.at/ajkq9',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'Google pixel Max',
+    price: 399.99,
+    img: 'shorturl.at/ajkq9',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Xiaomi',
+    price: 999.99,
+    img: 'shorturl.at/ajkq9',
+    amount: 1,
+  },
+];
 
 const TestReducer1 = props => {
   return (
     <Fragment>
       <p> hola mundo </p>
+
+      <div className="container">
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+      </div>
     </Fragment>
   );
 };
