084beef25bc51c4404b44e4074033b924eff33bb
sirfreedom <alemillan22@gmail.com>
2023-02-24 20:02:29 +0200

    player
    player

--- src/Components/PlaySong.jsx
+++ src/Components/PlaySong.jsx
@@ -1,80 +1,102 @@
+import React, {useState, useEffect} from 'react'
 
-/*
-import React from 'react';
-
-
-import {
-  BarSongTitle,
-  BottomBar,
-  Button,
-  PlayList,
-  Song,
-  SongTitle,
-} from './styles.js';
-*/
-//import { songList } from './constants.js';
-
-/*
 const buttonLabels = ['Not replaying', 'Replaying all', 'Replaying one'];
-const songList = [
-  {
-    "title": "Bohemian Rhapsody",
-    "author": "Queen",
-    "id": "1"
-  },
-  {
-    "title": "Stairway to Heaven",
-    "author": "Led Zeppelin",
-    "id": "2"
-  },
-  {
-    "title": "Smells Like Teen Spirit",
-    "author": "Nirvana",
-    "id": "3"
-  },
-  {
-    "title": "Hotel California",
-    "author": "Eagles",
-    "id": "4"
-  }
-];
 
 
-const PlayerProvider = ({ children }) => {
-  return children;
-};
 
-//const usePlayerContext = () => {};
 
-const ControlBar = () => {
-  const title = 'test - song';
-  return (
-    <BottomBar>
-      <BarSongTitle data-testid="barTitle">{title}</BarSongTitle>
-      <div>
-        <Button data-testid="previousButton">Previous</Button>
-        <Button data-testid="nextButton">Next</Button>
-        <Button data-testid="currentModeButton">{'replay mode'}</Button>
-      </div>
-    </BottomBar>
+const PlaySongs = () => {
+
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [playing, setPlaying] = useState(false);
+
+  const [SongList, setSongList] = useState
+  (
+    [
+      {
+        "title": "Bohemian Rhapsody",
+        "author": "Queen",
+        "id": "1",
+        "time" : 0.05
+      },
+      {
+        "title": "Stairway to Heaven",
+        "author": "Led Zeppelin",
+        "id": "2",
+        "time" : 0.03
+      },
+      {
+        "title": "Smells Like Teen Spirit",
+        "author": "Nirvana",
+        "id": "3",
+        "time" : 0.03
+      },
+      {
+        "title": "Hotel California",
+        "author": "Eagles",
+        "id": "4",
+        "time" : 0.03
+      }
+    ]
   );
-};
 
-const Songs = () => {
+  const currentTrack = SongList[currentTrackIndex];
+
+  const togglePlay = () => {
+    setPlaying(!playing);
+  };
+
+  const prevTrack = () => {
+    if (currentTrackIndex > 0) {
+      setCurrentTrackIndex(currentTrackIndex - 1);
+    }
+  };
+
+  const nextTrack = () => {
+    if (currentTrackIndex < SongList.length - 1) {
+      setCurrentTrackIndex(currentTrackIndex + 1);
+    }
+  };
+
   return (
     <>
-      {songList.map(({ title, author, id }) => (
-        <Song key={id}>
-          <SongTitle data-testid={id} active={false}>
-            {title}
-          </SongTitle>
-          <p>{author}</p>
-        </Song>
-      ))}
+   <div className="player">
+      <div className="player-track">
+        <div className="track-title">{currentTrack.title}</div>
+        <div className="track-artist">{currentTrack.artist}</div>
+      </div>
+      <div className="player-controls">
+        <button className="player-btn" onClick={prevTrack}>
+          Prev
+        </button>
+        <button className="player-btn" onClick={togglePlay}>
+          {playing ? 'Pause' : 'Play'}
+        </button>
+        <button className="player-btn" onClick={nextTrack}>
+          Next
+        </button>
+      </div>
+      <div className="music-list">
+        {SongList.map((track, index) => (
+          <div
+            key={track.title}
+            className={
+              index === currentTrackIndex
+                ? 'music-list-item active'
+                : 'music-list-item'
+            }
+            onClick={() => setCurrentTrackIndex(index)}
+          >
+            <div>{track.title}</div>
+            <div>{track.author}</div>
+          </div>
+        ))}
+      </div>
+    </div>
     </>
+
   );
 };
 
-export { PlayerProvider, Songs, ControlBar };
+export default PlaySongs;
 
-*/
